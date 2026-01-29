56b4499407af5bacf29aad5877f45aa8913603ae
fahmi <betun7096@gmail.com>
2026-01-29 13:28:32 +0630

    yah

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Promosi Mobil - Showroom Kendaraan Terpercaya</title>
     <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
-    
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 
